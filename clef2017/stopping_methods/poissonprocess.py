26ff6c1fd77577478f5ce54d355e83d1355bf0eb
william <william@COMSCIAD.DCS.SHEF.AC.UK>
2018-06-08 14:51:37 +0100

    up

--- clef2017/stopping_methods/poissonprocess.py
+++ clef2017/stopping_methods/poissonprocess.py
@@ -15,13 +15,18 @@ from sklearn.gaussian_process.kernels import WhiteKernel, ExpSineSquared
 import sys, getopt
 import math
 import random
+import pandas as pd
+import string
 
 def nextTime(rateParameter):
     return -math.log(1.0 - random.random()) / rateParameter
 
+def func(rate, x):
+    return 1- math.exp(-rate * x)
+
 
 qrel = ""
-file = ""
+files = []
 
 
 opts, args = getopt.getopt(sys.argv[1:],"hc:i:q")
@@ -32,56 +37,89 @@ for opt, arg in opts:
     elif opt in ("-q"):
         qrel = arg
     elif opt in ("-i"):
-        file = arg
-
+        files = arg.split('$')
 
 
 sampleRate = 3
+showtimeLine =True
 
-for filename in os.listdir(file):
-    scores = []
-    X_samps = []
-    c = 0
 
+
+#Loop every topic
+for filename in os.listdir(files[0]):
+
+    plots = []
     #its a folder skip over
-    if os.path.isdir(file + "\\" + filename):
+    if os.path.isdir(files[0] + "\\" + filename):
         continue
 
-    for line in open(file + "\\" + filename , "r"):
-        c = c + sampleRate
-        tmp = []
-        for val in re.split(r'\t+', line):
-            if val == "\n":
-                continue
 
-        #if float(line) not in scores:
-            tmp.append(float(val))
+    #all documents in sample
+    for percentage in np.linspace(1, 1, 1):
 
-        scores.append(np.array(tmp)) # y-axis
-        X_samps.append(float(c))     # x-axis
+        dist = 1
+        plt.title(filename)
+
+        #loop every file in input paremter
+        for file in files:
+
+            #read file into memory
+            scores = []
+            X_samps = []
+            c = 0
+            fileContent = open(file + "\\" + filename , "r").readlines()
+
+            for line in fileContent[0:int(len(fileContent) * percentage)]:
+                c = c + sampleRate
+                tmp = []
+                for val in re.split(r'\t+', line):
+                    if val == "\n":
+                        continue
+
+            #if float(line) not in scores:
+                    tmp.append(float(val))
+
+                scores.append(np.array(tmp)) # y-axis
+                X_samps.append(float(c))     # x-axis
 
     
-    scores = np.rot90(scores)
-    firstSet = scores[0]
-    x_points = []
-    y_points = []
-    lastPoint = None
+            scores = np.rot90(scores)
+            firstSet = scores[0]
+            x_points = []
+            y_points = []
+            
+            #crates a sample distrubtion of data in following format:
+            #x: 3, 18, 56, 91, 200, etc
+            #y: 1, 2, 3, 4, 5, 6
+            lastPoint = None
+            for x, y in zip(X_samps, firstSet): 
+                if lastPoint != y:
+                    lastPoint = y
+                    x_points.append(x)
+                    y_points.append(y)
 
-    for x, y in zip(X_samps, firstSet): 
-       if lastPoint != y:
-            lastPoint = y
-            x_points.append(x)
-            y_points.append(y)
 
+            #rate of documents parameter
+            rateOfDocument = len(x_points) / len(X_samps)
+            print("Estimated Rate of Document: " + str(rateOfDocument))
 
-    
-    print(x_points)
-    print(y_points)
-    rateOfDocument = len(x_points) / len(X_samps)
-    print("Estimated Rate of Document: " + str(rateOfDocument))
-    
+            if showtimeLine:
+                plt.ylim([0,10])
+                plt.ylabel("Runs")
+                plt.xlabel('Number of Documents in Sample')
+                plt.scatter(x_points, [dist for x in x_points], s=40, label=file.split('\\')[-1])
+                dist = dist + 1
+                plt.legend()
 
-    exit()    
+            else:
+                x = np.linspace(0, len(fileContent) * sampleRate, len(fileContent) * sampleRate)
+                y = np.array([func(rateOfDocument, X) for X in x])
+                plt.plot(x, y, label=file.split('\\')[-1])
+                plt.legend()
+
+        plt.show()
+
+   
 
 
 
